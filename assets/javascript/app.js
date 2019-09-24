b92f9c7ceb6cb2ef2ffbbc2674888c61ef0a4de1
kima111 <kima111@gmail.com>
2019-09-24 13:03:57 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,115 +1,163 @@
 
       $(document).ready(function() {
      
-      var delayButtonAlert;
       var questAnswers = {
           question1 : {
             q1:"What is the color of the sky?",
             a1:"blue",
-            w2:"red",
-            w3:"green",
-            w4:"apple"
+            a2:"red",
+            a3:"green",
+            a4:"apple"
           },
           question2 : {
             q1:"What color is grass?",
             a1:"green",
-            w2:"black",
-            w3:"white",
-            w4:"corn"
+            a2:"black",
+            a3:"white",
+            a4:"corn"
           },
           question3 : {
-            q1:"What is the color of the sky?",
-            a1:"3",
-            w2:"3",
-            w3:"3",
-            w4:"3"
+            q1:"What are my shoes?",
+            a1:"black",
+            a2:"red",
+            a3:"yellow",
+            a4:"grapes"
           },
           question4 : {
-            q1:"What is the color of the sky?",
-            a1:"4",
-            w2:"4",
-            w3:"4",
-            w4:"4"
+            q1:"Who was the first president?",
+            a1:"George Washington",
+            a2:"Michael Jordan",
+            a3:"Abraham Lincoln",
+            a4:"Thomas Adams"
           },
           question5 : {
-            q1:"What is the color of the sky?",
-            a1:"5",
-            w2:"5",
-            w3:"5",
-            w4:"5"
+            q1:"What is the capitol of Washington?",
+            a1:"Olympia",
+            a2:"Honolulu",
+            a3:"Seattle",
+            a4:"Tacoma"
           },
       };
-      var time = 30;
+      var time = 31;
       var questionNumber = 1;
-    
+      var correctAnswers = 0;
+      var answer1;
+      var answer2;
+      var answer3;
+      var answer4;
       //event listener is so that it can pick up on the event and not just when the document loads
 
-      document.addEventListener("click", function(event){
+      document.addEventListener("click", function(){
       $("#ansChoice1").on("click", function(){
+        if(answer1 == "a1"){
+          correctAnswers ++;
+        }
+        else{
+          console.log("wrong answer");
+        }
+      
         questionNumber ++;
         resetClock();
         displayChoices(questionNumber);
       });
  
       $("#ansChoice2").on("click", function(){
+        if(answer2 == "a1"){
+          correctAnswers ++;
+        }
+        else{
+          console.log("wrong answer");
+        }
+      
         questionNumber ++;
         resetClock();
         displayChoices();
       });
 
       $("#ansChoice3").on("click", function(){
+        if(answer3 == "a1"){
+          correctAnswers ++;
+        }
+        else{
+          console.log("wrong answer");
+        }
+      
         questionNumber ++;
         resetClock();
         displayChoices();
       });
 
       $("#ansChoice4").on("click", function(){
+        if(answer4 == "a1"){
+          correctAnswers ++;
+        }
+        else{
+          console.log("wrong answer");
+        }
+      
         questionNumber ++;
         resetClock();
         displayChoices();
       });
 
     });
+
       $("#start").on("click", function() {
-   
         intervalId = setInterval(count, 1000);
         clockRunning = true;
-      
-        
         $("#start").fadeOut();
-        var currentQ = "question" + questionNumber;
-        var q = "q1"
-        console.log(currentQ);
-        $("#question").text(questAnswers[currentQ][q]);
+        displayChoices();
         
-       if (time == 0){
-         questionNumber ++;
-       }
-       displayChoices(questionNumber);
       });
 
       function resetClock(){
-        time = 30;
+        time = 31;
       }
- 
-      function displayChoices(){
+
+      //used to shuffle array
+      var shuffle = function (array) {
+
+        var currentIndex = array.length;
+        var temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
       
+        return array;
+      
+      };
+
+      function displayChoices(){
+        var ansArray = [1,2,3,4];
+        var shuffledAnsArray = shuffle(ansArray);
         var questionNum = "question" + questionNumber;
-        var answer1 = "a1";
-        var answer2 = "w2";
-        var answer3 = "w3";
-        var answer4 = "w4";
+        var currentQuestion = "q1";
+    
+        answer1 = "a" + (shuffledAnsArray[0]).toString();;
+        answer2 = "a" + (shuffledAnsArray[1]).toString();
+        answer3 = "a" + (shuffledAnsArray[2]).toString();
+        answer4 = "a" + (shuffledAnsArray[3]).toString();
+  
+        var currentQuestionText = questAnswers[questionNum][currentQuestion];
+
         var firstAnswer = questAnswers[questionNum][answer1];
         var secondAnswer = questAnswers[questionNum][answer2];
         var thirdAnswer = questAnswers[questionNum][answer3];
         var fourthAnswer = questAnswers[questionNum][answer4];
 
-        var choices = "<button id='ansChoice1'>" + firstAnswer + "</button></br>" +
+        var questionChoices =
+        "<text id ='question'>" + currentQuestionText + "</text></br>" + 
+        "<button id='ansChoice1'>" + firstAnswer + "</button></br>" +
         "<button id='ansChoice2'>" + secondAnswer + "</button></br>" +
         "<button id='ansChoice3'>" + thirdAnswer + "</button></br>" +
         "<button id='ansChoice4'>" + fourthAnswer + "</button></br>"
         ;
-        $("#choices").html(choices);
+        $("#choices").html(questionChoices);
 
       }
    
@@ -117,8 +165,11 @@
         time--;
         var converted = timeConverter(time);
         $("#timer").text(converted);
-      
-
+        if(time == 0){
+          questionNumber ++;
+          resetClock();
+          displayChoices();
+        }
       }
       function timeConverter(t) {
 
