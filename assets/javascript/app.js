548f5cff383489f0fb821911da03f5772ab978ad
kima111 <kima111@gmail.com>
2019-09-25 09:12:22 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -192,5 +192,5 @@
         }
         return minutes + ":" + seconds;
       }
-
+      (function(l){var i,s={touchend:function(){}};for(i in s)l.addEventListener(i,s)})(document); // sticky hover fix in iOS
     });
