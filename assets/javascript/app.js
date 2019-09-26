9fbf2bbc2a36a507193a789a921886c33f20dd90
kima111 <kima111@gmail.com>
2019-09-26 10:36:55 -0700

    final update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,10 @@
       var questAnswers = {
           question1 : {
             q1:"How many cups do you have?",
-            a1:"Only 1",
-            a2:"less than 10",
-            a3:"less than 20",
-            a4:"I lost count"
+            a1:"My one and only",
+            a2:"5-20",
+            a3:"Over 100",
+            a4:"I just use paper cups"
           },
           question2 : {
             q1:"How many apps do you have?",
@@ -25,7 +25,7 @@
           },
           question4 : {
             q1:"What do you do when you have to get rid of stuff",
-            a1:"Sell then donate",
+            a1:"Sell my stuff then donate what's left",
             a2:"Garage sell and keep the rest",
             a3:"Why get rid of stuff?",
             a4:"Never get rid of just repurpose"
@@ -33,9 +33,9 @@
           question5 : {
             q1:"What do you do with plastic bags",
             a1:"I have my own re-usable bag",
-            a2:"save them",
-            a3:"what paper only!",
-            a4:"recycle them"
+            a2:"Save them of course!",
+            a3:"what!? paper only!",
+            a4:"Recycle them"
           },
           question6 : {
             q1:"If you had to choose a number what would it be",
@@ -52,11 +52,11 @@
             a4:"I need another room for the books"
           },
           question8 : {
-            q1:"What does minimalism mean",
-            a1:"only keep the stuff you need and enjoy",
-            a2:"get rid of everything",
-            a3:"only keep things you need",
-            a4:"evil"
+            q1:"What does minimalism mean?",
+            a1:"Only keep the stuff you need and enjoy",
+            a2:"Get rid of everything",
+            a3:"Only keep things you need",
+            a4:"Evil incarnate"
           }
       };
       var time = 30;
@@ -68,6 +68,7 @@
       var answer3;
       var answer4;
       var questionArrayLength = Object.keys(questAnswers).length + 1;
+      $("#startAgain").hide();
    
       //event listener is so that it can pick up on the event and not just when the document loads
       document.addEventListener("click", function(event)
@@ -76,41 +77,45 @@
       $("#ansChoice1").on("click", function(event){
         event.preventDefault();
         if(answer1 == "a1"){
+          displayRightScreen();
           correctAnswers ++;
         }
-        questionNumber ++;
-        resetClock();
-        displayChoices(questionNumber);
+        else if(answer1 !== "a1"){
+          displayWrongScreen();
+        }
       });
  
       $("#ansChoice2").on("click", function(event){
         event.preventDefault();
         if(answer2 == "a1"){
+          displayRightScreen();
           correctAnswers ++;
         }
-        questionNumber ++;
-        resetClock();
-        displayChoices();
+        else if(answer2 !== "a1"){
+          displayWrongScreen();
+        }
       });
 
       $("#ansChoice3").on("click", function(event){
         event.preventDefault();
         if(answer3 == "a1"){
+          displayRightScreen();
           correctAnswers ++;
         }
-        questionNumber ++;
-        resetClock();
-        displayChoices();
+        else if(answer3 !== "a1"){
+          displayWrongScreen();
+        }
       });
 
       $("#ansChoice4").on("click", function(event){
         event.preventDefault();
         if(answer4 == "a1"){
+          displayRightScreen();
           correctAnswers ++;
         }
-        questionNumber ++;
-        resetClock();
-        displayChoices();
+        else if(answer4 !== "a1"){
+          displayWrongScreen();
+        }
       });
 
     });
@@ -122,11 +127,8 @@
           $("#timer").text(converted);
 
           if(time == 0){
-            questionNumber ++;
-            resetClock();
-            displayChoices();
+            displayOutOfTimeScreen();
           }
-
         }
       }
 
@@ -145,7 +147,7 @@
       });
 
       function resetClock(){
-        time = 31;
+        time = 30;
       }
 
       //used to shuffle array
@@ -165,9 +167,58 @@
         return array;
       
       };
-
+      function displayRightScreen(){
+        var questionNum = "question" + questionNumber;
+        $("#timer").hide();
+        $("#choices").hide();
+        $("#screenArea").show();
+        $("#screenArea").html("YOU GOT IT!" + "<br>" + "The correct choice was: " + questAnswers[questionNum]["a1"]);
+        setTimeout(function(){
+          $("#timer").text("00:30");
+          $("#choices").show();
+          $("#timer").show();
+          questionNumber ++;
+          resetClock();
+          displayChoices(questionNumber);
+         
+        }, 1000 * 3);
+        
+        
+      }
+      function displayWrongScreen(){
+        var questionNum = "question" + questionNumber;
+        $("#timer").hide();
+        $("#choices").hide();
+        $("#screenArea").show();
+        $("#screenArea").html("WRONG!" + "<br>" + "The correct choice was: " + questAnswers[questionNum]["a1"]);
+        setTimeout(function(){
+          $("#timer").text("00:30");
+          $("#choices").show();
+          $("#timer").show();
+          questionNumber ++;
+          resetClock();
+          displayChoices(questionNumber);
+         
+        }, 1000 * 3);
+      }
+      function displayOutOfTimeScreen(){
+        var questionNum = "question" + questionNumber;
+        $("#timer").hide();
+        $("#choices").hide();
+        $("#screenArea").show();
+        $("#screenArea").html("OUT OF TIME!" + "<br>" + "The correct choice was: " + questAnswers[questionNum]["a1"]);
+        setTimeout(function(){
+          $("#timer").text("00:30");
+          $("#choices").show();
+          $("#timer").show();
+          questionNumber ++;
+          resetClock();
+          displayChoices(questionNumber);
+         
+        }, 1000 * 3);
+      }
       function displayChoices(){
-  
+        $("#screenArea").hide();
         if(questionNumber !== questionArrayLength){
           var ansArray = [1,2,3,4];
           var shuffledAnsArray = shuffle(ansArray);
@@ -198,11 +249,37 @@
         else{
           clockRunning=false;
           $("#timer").hide();
+          $("#startAgain").show();
+          $("#startAgain").on("click", function(){
+            startAgain();
+            
+          })
+          
           var score = "<h2>" + (correctAnswers).toString() + " out of " + (Object.keys(questAnswers).length.toString()) + " correct </h2>";
           $("#choices").html(score);
         }
       }
-
+      function startAgain(){
+       time = 30;
+       questionNumber = 1;
+       correctAnswers = 0;
+       clockRunning = false;
+       answer1 = "";
+       answer2 = "";
+       answer3 = "";
+       answer4 = "";
+       questionArrayLength = Object.keys(questAnswers).length + 1;
+       $("#startAgain").hide();
+       $(".main-container").attr('style','border-style:solid');
+            $("#start").hide();
+            $("#timer").show();
+            $("#timer").text("00:30");
+            if(!clockRunning){
+            clockRunning = true;
+            }
+           
+            displayChoices();
+      }
       function timeConverter(t) {
 
         var minutes = Math.floor(t / 60);
